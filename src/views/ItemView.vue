aba1a1811e2fa565e89f3879f6a1a0cf2199d75d
Doobyeol <doobyeol@gmail.com>
2022-04-20 23:58:02 +0900

    질문 상세 페이지 스타일링, v-html 디렉티브

--- src/views/ItemView.vue
+++ src/views/ItemView.vue
@@ -1,17 +1,39 @@
 <template>
   <div>
-    <p>name : {{ itemInfo.id }}</p>
-    <p>title : {{ itemInfo.title }}</p>
-    <p>{{ itemInfo.content }}</p>
+    <section>
+      <!-- 질문 상세 정보 -->
+      <div class="user-container">
+        <div><i class="fa-solid fa-user"></i></div>
+        <div class="user-description">
+          <router-link :to="`/user/${fetchedItem.id}`">
+            {{ fetchedItem.user }}
+          </router-link>
+          <div class="time">{{ fetchedItem.time_ago }}</div>
+        </div>
+      </div>
+      <h2>{{ fetchedItem.title }}</h2>
+    </section>
+    <section>
+      <!-- 질문 댓글 -->
+      <div v-html="fetchedItem.content">
+        <!-- {{ fetchedItem.content }} -->
+      </div>
+    </section>
+    <!-- <p>name : {{ fetchedItem.id }}</p>
+    <p>title : {{ fetchedItem.title }}</p>
+    <p>{{ fetchedItem.content }}</p> -->
   </div>
 </template>
 
 <script>
+import { mapGetters } from "vuex";
+
 export default {
   computed: {
-    itemInfo() {
-      return this.$store.state.item;
-    },
+    ...mapGetters(["fetchedItem"]),
+    // itemInfo() {
+    //   return this.$store.state.item;
+    // },
   },
   created() {
     const itemId = this.$route.params.id;
@@ -21,4 +43,19 @@ export default {
 };
 </script>
 
-<style></style>
+<style scoped>
+.user-container {
+  display: flex;
+  align-items: center;
+  padding: 0.5rem;
+}
+.fa-user {
+  font-size: 2.5rem;
+}
+.user-description {
+  padding-left: 8px;
+}
+.time {
+  font-size: 0.7rem;
+}
+</style>
